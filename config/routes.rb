482079102bf23c10a737cf5dddcce712bb40bcc3
YourName <you@example.com>
2020-04-26 10:58:40 +0900

    user-kihon2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   root to: 'homes#index'
   resources :homes, only: [:new]
 
+  # root to: 'products#index'
+  resources :signup do
+    collection do
+      get 'index'
+      get 'member'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
